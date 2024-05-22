db1cab7aa7cc22564da28ba458e78f3f7b2cc449
TerenceL-TL <mirac318@outlook.com>
2024-05-23 03:53:44 +0800

    The RARS Program of Lab1(Part2)

--- lab1/RARS/ex1.asm
+++ lab1/RARS/ex1.asm
@@ -23,7 +23,6 @@ MAIN:
 	lui s0, 0x10010   # original string address
 	addi s1, s0, 17   # pattern string address
 	addi t4, s0, 0   
-	addi t5, s0, 3
 	addi t6, s1, 13   # signs the end of original str
 	addi s0, s0, -1   # move s0 for program complement
 	jal ra, find_substr
@@ -34,6 +33,7 @@ find_substr:
 	push s1           # save registers into the stack
 itering:
 	addi s0, s0, 1
+	addi t5, s0, 3
 	beq s0, t6, failed
 	
 	addi t0, s0, 0
@@ -42,7 +42,7 @@ loop:
 	lb t2, 0(t0)
 	lb t3, 0(t1)
 	bne t2, t3, itering
-	beq t1, t5, found
+	beq t0, t5, found
 	addi t0, t0, 1
 	addi t1, t1, 1
 	jal zero, loop
